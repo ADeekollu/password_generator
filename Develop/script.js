55c1ddba0874211a9a39bea50bdb6067fbb6c04c
ADeekollu <115489389+ADeekollu@users.noreply.github.com>
2022-10-27 17:39:47 -0400

    added if statement to password criteria and for loops

--- Develop/script.js
+++ Develop/script.js
@@ -5,6 +5,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+function randomInt(min, max) {
+  return Math.floor(Math.random()*(max - min) + min)
+}
+
+function randomItem(list) {
+  return list[randomInt(0, passwordCriteria - 1)]
+}
+
+
 function generatePassword() {
 
 
@@ -22,10 +32,46 @@ function generatePassword() {
   return
   };
 
-  var wantNumber = window.confirm("Would you like numbers in your password?");
-  var wantSymbol = window.confirm("Would you like symbols in your password?");
-  var wantLowerCase = window.confirm("Would you like lower case letters in your password?");
-  var wantUpperCase = window.confirm("Would you like upper case in your password?");
+  var likeNumber = window.confirm("Would you like numbers in your password?");
+  var likeSymbol = window.confirm("Would you like symbols in your password?");
+  var likeLowerCase = window.confirm("Would you like lower case letters in your password?");
+  var likeUpperCase = window.confirm("Would you like upper case in your password?");
+
+  var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var symbolsList = ["!", "@", "#", "$", "&", "?", "%"];
+  var lowerCaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var upperCaseList = []
+
+  var passwordCriteria = []
+
+  for (var i = 0; i < lowerCaseList.length; i++) {
+    upperCaseList[i] = lowerCaseList[i].toUpperCase()
+  }
+
+  if (likeNumber === true) {
+    passwordCriteria.push(numberList)
+  }
+
+  if (likeSymbol === true) {
+    passwordCriteria.push(symbolsList)
+  }
+
+  if (likeLowerCase === true) {
+    passwordCriteria.push(lowerCaseList)
+  }
+
+  if (likeUpperCase === true) {
+    passwordCriteria.push(upperCaseList)
+  }
+
+  var createdPassword = ""
+
+  for (i = 0; i < passwordLength; i++) {
+    var randomList = randomItem(passwordCriteria)
+    var randomChar = randomItem(randomList)
+  }
+
+
 
 
 
