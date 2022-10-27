89fb162c31fd7c2f7ff542b1d199d55ace4feab6
ADeekollu <115489389+ADeekollu@users.noreply.github.com>
2022-10-27 16:40:16 -0400

    added window confirms for password criteria

--- Develop/script.js
+++ Develop/script.js
@@ -8,13 +8,26 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
 
 
-var userInput = window.prompt("How long do you want your password?");
+  var input = window.prompt("How long do you want your password?");
 
-var passwordLength = parseInt(userInput)
+  var passwordLength = parseInt(input);
 
-if (isNaN(passwordLength)){
-  window.alert("Please enter a number.")
-}
+  if (isNaN(passwordLength)){
+    window.alert("Please enter a number.")
+  return
+  };
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Password must be between 8 and 128 characters")
+  return
+  };
+
+  var wantNumber = window.confirm("Would you like numbers in your password?");
+  var wantSymbol = window.confirm("Would you like symbols in your password?");
+  var wantLowerCase = window.confirm("Would you like lower case letters in your password?");
+  var wantUpperCase = window.confirm("Would you like upper case in your password?");
+
+
 
 
 }
